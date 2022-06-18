5530d9197bc05639831fe1148ba34bd1d105907b
Thainara Carvalho <thai.cds1@gmail.com>
2022-06-18 11:36:59 -0400

    implementando propriedades de 2 a 7

--- src/main/java/MathUtil.java
+++ src/main/java/MathUtil.java
@@ -1,12 +1,42 @@
 public class MathUtil {
-    public static int mdc(int a, int b){
+    public static double mdc(double a, double b){
+        //IMPLEMENTAR O CODIGO, TESTAR E REFATORAR. PASSO ADICIONAL: TESTAR NOVAMENTE.
+        //P7 - Tira o sinal das variáveis
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        //P6 - ordena as variaveis colocando o maior valor em a
+        final double min = Math.min(a,b);
+        a = Math.max(a,b);
+        b = min;
+
         //P1 E P2
-        if (b > 0 && a % b == 0)
+        if (b > 0 && a % b == 0) {
             return b; //alt + enter para refatorar
+        }
 
+        //P3
+        if( b == 0) {
+            return Math.abs(a); //abs == valor absoluto
+        }
 
+        //TODO: P12 - se chegou aqui, considera que o número é primo, mas nao resolve todos os problemas ainda.
 
+        return mdc(a-b, b);
+    }
 
-        return -1;
+    // ... - varargs = variable arguents = parametros variaveis (quantidade)
+    public static double mdc(double ...valores){
+        double a = valores[0];
+        //TODO: valores.fori = cria o for de valores
+        /*for (int i = 1; i < valores.length; i++) {
+            final double b = valores[i];
+            a = mdc(a,b);
+        }*/
+        for (final double b : valores) {
+            a = mdc(a,b);
+
+        }
+        return a;
     }
 }
--- src/test/java/MathUtilTest.java
+++ src/test/java/MathUtilTest.java
@@ -1,24 +1,122 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static  pacote.MathUtil.*;
 
 class MathUtilTest {
 
     @Test
     void mdcP1Pares() { //testando numeros pares
-        final int a = 8, b = 2, esperado = b;
-        final int obtido = MathUtil.mdc(a,b);
+        final double a = 8, b = 2, esperado = b;
+        final double obtido = MathUtil.mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
+    @Test
+    void mdcP1NumerosIguais() { //testando numeros iguais
+        final double a = 4, b = 4, esperado = b;
+        final double obtido = MathUtil.mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
+    @Test
+    void mdcP2(){
+        final double a = 8, b = 4, divisor = 2;
+        //final double esperado = 2;
+        // o valor obtido é sempre o resultado da chamada
+        //da funçao sendo testada
+        final double obtido = MathUtil.mdc(a,b);
+        //assertEquals(0, obtido % divisor);
+        assertTrue(obtido % divisor == 0); //quando se tem uma condição maior
+        // |a| == valor positivo de a
+    }
+    @Test
+    void mdcP3(){
+        final double a = 5, esperado = a;
+        final double obtido = MathUtil.mdc(a, 0);
         assertEquals(esperado, obtido);
     }
 
     @Test
-    void testeMdcAImpar(){
-        final int a = 9;
-        final int b = 3;
-        final int esperado = 3;
-        final int obtido = MathUtil.mdc(a,b);
+    void mdcP3Positivo(){
+        final double a = 5, esperado = a;
+        final double obtido = MathUtil.mdc(a,0);
         assertEquals(esperado, obtido);
+
     }
 
+    @Test
+    void mdcP3Negativo(){
+        final double a = -5, esperado = 5;
+        final double obtido = MathUtil.mdc(a,0);
+        assertEquals(esperado, obtido);
+
+    }
+    @Test
+    void mdcP4(){
+        final double m = 5, a = 6, b = 3;
+        final double esperado = MathUtil.mdc(m*a, m*b);
+        final double obtido = m * MathUtil.mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
+    @Test
+    void mdcP6(){
+        final double a = 8, b=4;
+        final double esperado = MathUtil.mdc(a, b);
+        final double obtido = MathUtil.mdc(b, a);
+        assertEquals(esperado, obtido);
+        //se valor deo actual der -1 significa que não há implementação para o teste
+    }
+
+    @Test
+        //se os numeros são primos não obte mdc, vai ser 1
+    void mdcP7(){
+        final double a = 12, b = 3;
+        final double esperado = MathUtil.mdc(-a,b);
+        final double obtido = MathUtil.mdc(a, -b);
+        assertEquals(esperado, obtido);
+    }
+    @Test
+    void mdcP7Primos(){
+        final double a = 7, b = 3;
+        final double esperado = MathUtil.mdc(-a,b);
+        final double obtido = MathUtil.mdc(a, -b);
+        assertEquals(esperado, obtido);
+        System.out.println("esperado: " + esperado + " obtido: " + obtido);
+
+    }
+    @Test
+        //quando se tem 1 funcao, o resultado é o "obtido" da funcao
+    void mdcP12UmNumeroPrimo(){
+        final double p = 7;
+        final double a = 2;
+        //mdc(p,a) == p ou 1
+        final double obtido = MathUtil.mdc(p ,a);
+        assertEquals(1, obtido);
+    }
+
+    @Test
+    void mdcP12UmNumeroPrimoIguais(){
+
+        final double p = 7;
+        final double a = 7;
+        //mdc(p,a) == p ou 1
+        final double obtido = MathUtil.mdc(p ,a);
+        System.out.println("esperado: " + p + " obtido: " + obtido);
+        assertEquals(p, obtido);
+    }
+    @Test  //calculando o mdc até que os nueros sejam iguais
+    void mdcCasoGeralPares() { //testando numeros pares
+        final double a = 12, b = 8, esperado = 4;
+        final double obtido = MathUtil.mdc(a,b);
+        assertEquals(esperado, obtido);
+    }
+    @Test
+    void mdcMultiplosValores(){
+        //MathUtil.mdc(12,8,4); passando individualmente
+        //final double[] valores = {12,8,4};  //passando em vetor
+        final double esperado = 4;
+        final double obtido = MathUtil.mdc(12,8,4);
+        assertEquals(esperado, obtido);
+        //MathUtil.mdc(valores);
+    }
 }
