f626001bfd5a95ff49e65e632c9688df7f3e33ea
Thainara Carvalho <thai.cds1@gmail.com>
2022-06-18 11:15:33 -0400

    teste valores pares e impares

--- src/main/java/MathUtil.java
+++ src/main/java/MathUtil.java
@@ -1,13 +1,9 @@
 public class MathUtil {
-    public static double mdc(double a, double b){
+    public static int mdc(int a, int b){
         //P1 E P2
         if (b > 0 && a % b == 0)
             return b; //alt + enter para refatorar
 
-        //P3
-        if( b == 0)
-            return Math.abs(a); //abs == valor absoluno
-             //return a * -1;
 
 
 
--- src/test/java/MathUtilTest.java
+++ src/test/java/MathUtilTest.java
@@ -7,48 +7,18 @@ class MathUtilTest {
 
     @Test
     void mdcP1Pares() { //testando numeros pares
-        final double a = 8, b = 2, esperado = b;
-        final double obtido = MathUtil.mdc(a,b);
-        assertEquals(esperado, obtido);
-    }
-    @Test
-    void mdcP1NumerosIguais() { //testando numeros iguais
-        final double a = 4, b = 4, esperado = b;
-        final double obtido = MathUtil.mdc(a,b);
-        assertEquals(esperado, obtido);
-    }
-    @Test
-    void mdcP2(){
-        final double a = 8, b = 4, divisor = 2;
-        //final double esperado = 2;
-        // o valor obtido é sempre o resultado da chamada
-        //da funçao sendo testada
-        final double obtido = MathUtil.mdc(a,b);
-        //assertEquals(0, obtido % divisor);
-        assertTrue(obtido % divisor == 0); //quando se tem uma condição maior
-        // |a| == valor positivo de a
-    }
-    @Test
-    void mdcP3(){
-        final double a = 5, esperado = a;
-        final double obtido = MathUtil.mdc(a, 0);
+        final int a = 8, b = 2, esperado = b;
+        final int obtido = MathUtil.mdc(a,b);
         assertEquals(esperado, obtido);
     }
 
     @Test
-    void mdcP3Positivo(){
-        final double a = -5, esperado = a *-1;
-        final double obtido = MathUtil.mdc(a,0);
+    void testeMdcAImpar(){
+        final int a = 9;
+        final int b = 3;
+        final int esperado = 3;
+        final int obtido = MathUtil.mdc(a,b);
         assertEquals(esperado, obtido);
-
-    }
-
-    @Test
-    void mdcP3Negativo(){
-        final double a = -5, esperado = a *-1;
-        final double obtido = MathUtil.mdc(a,0);
-        assertEquals(esperado, obtido);
-
     }
 
 }
